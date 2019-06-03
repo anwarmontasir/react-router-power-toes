dbfb71ae42bae08b42ba6a4ea4ba69f618571fe7
Anwar Montasir <anwarmontasir@gmail.com>
2019-06-03 09:56:19 -0700

    setup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Nav from './Nav';
+import Header from './Header';
+import Footer from './Footer';
+import HomePage from './HomePage';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  render() {
+      return (
+        <div className='App'>
+          <nav><Nav /></nav>
+          <header><Header /></header>
+          <main><HomePage /></main>
+          <footer><Footer /></footer>
+        </div>
+    );
+  }
 }
-
-export default App;
